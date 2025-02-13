a086de18a695ba0d0b95bfa4feb7679a03081951
kaminno <49490942+kaminno@users.noreply.github.com>
2025-02-13 18:22:02 +0100

    unused files removed

--- src/Hello2.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface Hello2 extends Remote {
-	// public String sayHello(String clientName)  throws RemoteException;
-	public String sayHello(Accounting_itf client)  throws RemoteException;
-}
--- src/Registry_itf.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;
-
-public interface Registry_itf extends Remote {
-    public void register(Accounting_itf client) throws RemoteException;
-}
